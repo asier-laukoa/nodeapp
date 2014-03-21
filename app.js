89f79c47a7c263dbdd8826ade216eecea5dc1464
Asier <asier@laukoa-studio.com>
2014-03-21 18:09:47 +0100

    EJS

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.get('/',function(req,res){
 });
 
 app.get('/about',function(req,res){
-	res.render('layout.ejs',{title: 'About Us',body:'<h1>About Us</h1>'});
+	res.render('layout.ejs',{title: 'About Us',body: '<h1>About Us</h1>'});
 });
 
 app.get('/*',function(req,res){
 	res.status(404).render('error.ejs',{title:"Error!"});
 });
 
-console.log('server started');
+console.log('Server started on localhost:3000');
 app.listen(3000);
